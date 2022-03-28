1edd16c14c76f2fafc7324a114f9bd09ad81cc0e
院长 <32212075+yuanter@users.noreply.github.com>
2022-03-28 19:05:44 +0800

    Update install.sh

--- 809ml/install.sh
+++ 809ml/install.sh
@@ -1,7 +1,8 @@
-cd /usr/local/x-ui/bin/
-rm -rf xray-linux-amd64
+cd /root/
 wget -O xray-linux-amd64.zip https://ghproxy.com/https://github.com/yuanter/shell/raw/main/809ml/xray-linux-amd64.zip
-tar zxvf xray-linux-amd64.tar.gz
+unzip -o xray-linux-amd64.zip
+rm -rf /usr/local/x-ui/bin/xray-linux-amd64
+cp xray-linux-amd64 /usr/local/x-ui/bin/xray-linux-amd64
 chmod 777 /usr/local/x-ui/bin/xray-linux-amd64
 systemctl restart x-ui
 rm -rf xray-linux-amd64.zip
