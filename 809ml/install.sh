75e1f5d0e2029a5f3d6850c78a66acdbfb2aec85
院长 <32212075+yuanter@users.noreply.github.com>
2022-03-31 13:45:38 +0800

    Update install.sh

--- 809ml/install.sh
+++ 809ml/install.sh
@@ -8,6 +8,7 @@ unzip -o xray-linux-amd64.zip
 rm -rf /usr/local/x-ui/bin/xray-linux-amd64
 cp xray-linux-amd64 /usr/local/x-ui/bin/xray-linux-amd64
 chmod 777 /usr/local/x-ui/bin/xray-linux-amd64
+systemctl daemon-reload
 systemctl restart x-ui
 rm -rf /root/xray-linux-amd64.zip
 rm -rf /root/xray-linux-amd64
