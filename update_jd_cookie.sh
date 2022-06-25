2ba296bc84c44fb1ef84a95a6da9bffa04a20b63
院长 <32212075+yuanter@users.noreply.github.com>
2022-06-25 19:41:09 +0800

    优化脚本

--- update_jd_cookie.sh
+++ update_jd_cookie.sh
@@ -5,6 +5,17 @@ docker pull yuanter/jd_cookie
 # 跳转至jd_cookie目录
 cd /root/jd_cookie
 # 移除容器
-docker rm -f jd_cookie
+id=docker ps | grep "jd_cookie" | awk '{print $1}'
+echo -e $docker_id
+if [ $id ];then
+  docker rm -f $id
+fi
+#docker rm -f jd_cookie
 #启动容器
 docker run -d --privileged=true --restart=always  --name jd_cookie -p 1170:1170  -v $PWD/application.yml:/application.yml --link redis:redis yuanter/jd_cookie
+#删除旧容器
+docker rmi $(docker images -f "dangling=true" -q)
+#删除脚本
+if [ -d "update_jd_cookie.sh" ];then
+  rm -rf update_jd_cookie.sh
+fi
