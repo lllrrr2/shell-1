b385550d36978869321872f89654c9ea5729a6af
院长 <32212075+yuanter@users.noreply.github.com>
2022-06-25 19:59:31 +0800

    Update update_jd_cookie.sh

--- update_jd_cookie.sh
+++ update_jd_cookie.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+#获取当前路径
+path=$PWD
+
 # 先更新镜像
 docker pull yuanter/jd_cookie
 # 跳转至jd_cookie目录
@@ -14,7 +17,7 @@ fi
 #启动容器
 docker run -d --privileged=true --restart=always  --name jd_cookie -p 1170:1170  -v $PWD/application.yml:/application.yml --link redis:redis yuanter/jd_cookie
 #删除脚本
-if [ -f "$PWD/update_jd_cookie.sh" ]; then
+if [ -f "$path/update_jd_cookie.sh" ]; then
   echo "文件存在"
-  rm -rf $PWD/update_jd_cookie.sh
+  rm -rf $path/update_jd_cookie.sh
 fi
